6c53065bd8f0f9366331b78836639784f179da20
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-14 21:46:03 +0000

    implemented timer to take in command line arguments and beep after the alloted time

---
+++ timer1.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2); //args ['arg1' 'arg2' ...]
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed
+// The user can specify an unlimited number of alarms using command line arguments
+
+//ignore cases where no arguments were typed
+if (args.length !== 0) {
+  //loop args, ignore negative numbers
+  for (seconds of args) {
+    //convert string elements to numbers
+    seconds = Number(seconds)
+    //ignore inputs that aren't a number, and are negative numbers
+    if (seconds !== NaN && seconds > 0) {
+      //after x seconds make beep noise; convert seconds into milliseconds by *1000
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, seconds*1000); 
+    }
+  }
+}
+
